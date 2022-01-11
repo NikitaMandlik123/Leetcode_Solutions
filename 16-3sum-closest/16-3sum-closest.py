345a4350d0d6360fee398aac74b06d0f2dbb3e6a
Nikita Mandlik <nmandli1@binghamton.edu>
2022-01-11 16:46:21 -0500

    Time: 418 ms (12.69%), Space: 14.3 MB (71.17%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -4,14 +4,14 @@ class Solution:
         nums.sort()
         ans = float('inf')
         for i in range(n-1):
-            l, r = i+1, n-1 # starting the search
+            l, r = i+1, n-1 
             while l < r:
                 temp_sum = nums[l] + nums[r] + nums[i]
-                # if we are above the target, reduce the bigger element
+
                 if temp_sum > target: r -= 1
-                # if we are below the target, increase the smaller element
+
                 elif temp_sum < target: l += 1
-                # if we are *at* the target, we have the best possible answer.
+
                 else: return temp_sum
                 ans = min(ans, temp_sum, key=lambda x: abs(target-x))
         return ans
