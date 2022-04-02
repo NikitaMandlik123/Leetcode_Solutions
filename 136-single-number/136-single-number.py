e89edd6dfb97032cd32c13844df1f3a21521c4d9
Nikita Mandlik <nmandli1@binghamton.edu>
2022-04-02 11:17:02 -0400

    Time: 128 ms (98.23%), Space: 17.1 MB (5.66%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            if(nums.count(i) == 1):
-                return(i)        
+        return sum(list((set(nums)))*2)- sum(nums)
+        
+        #time complexity : O(n)
+        #space complexity: O(n)
+        
