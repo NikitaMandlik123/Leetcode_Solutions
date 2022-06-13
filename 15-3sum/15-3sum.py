364a32b826ddca0a7dd499c6cedc3993146aec76
Nikita Mandlik <nmandli1@binghamton.edu>
2022-06-13 08:14:23 -0400

    Time: 3049 ms (24.24%), Space: 17.4 MB (87.66%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,42 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        sol = set([])
-        p = sorted([n for n in nums if n>0])
-        p_set = set(p)
-        z = [n for n in nums if n == 0]
-        neg = sorted([n for n in nums if n<0])
-        neg_set = set(neg)
-        
-        
-        
-   
-        if len(z)>=3:
-            sol.add((0,0,0))
+        nums.sort()
+        ret = set()
+        for i in range(len(nums)-1):
+            n3 = -nums[i]
+            l = i+1
+            r = len(nums)-1
             
-            
-    
-        if len(z)>0:
-            for n in neg:
-                if -n in p_set:
-                    sol.add((n,0,-n))
-                    
-                    
-                    
-     
-        n = len(neg)
-        for i in range(n):
-            for j in range(i+1,n):
-                diff = -(neg[i]+neg[j])
-                if diff in p_set:
-                    sol.add((neg[i],neg[j],diff))
-                    
-                    
-        
-        n = len(p)
-        for i in range(n):
-            for j in range(i+1,n):
-                diff = -(p[i]+p[j])
-                if diff in neg_set:
-                    sol.add((diff,p[i],p[j]))
-        return list(sol)        
+            while l<r:
+                if nums[l]+nums[r]<n3:
+                    l=l+1
+                elif nums[l]+nums[r]>n3:
+                    r =r-1
+                else:
+                    ret.add((nums[i],nums[l],nums[r]))
+                    l=l+1
+                    r = r-1
+        return ret
+                
+                
