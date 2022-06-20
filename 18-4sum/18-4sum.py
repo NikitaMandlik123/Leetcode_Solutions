4b3398d3d3020e8403cacfe1f76ffaf6a03cea31
Nikita Mandlik <nmandli1@binghamton.edu>
2022-06-20 11:43:50 -0400

    Time: 3870 ms (5.72%), Space: 13.9 MB (93.20%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,42 +1,24 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        i = 0
-        L = len(nums)
-        res = []
-        while i < L-3:
-            if target-nums[i] < 3*nums[i+1] or target-nums[i] > 3*nums[-1]:
-                    while i < L-4 and nums[i] == nums[i+1]:
-                        i+=1
-                    i+=1
-                    continue
-            j = i+1
-            while j < L-2:
-                if target-nums[i]-nums[j] < 2*nums[j+1] or target-nums[i]-nums[j] > 2*nums[-1]:
-                    while j < L-3 and nums[j] == nums[j+1]:
-                        j+=1
-                    j+=1
-                    continue
-                left = j+1
-                right = L-1
-                new_target = target-nums[i]-nums[j]
-                while left<right:
-                    if nums[left]+nums[right] > new_target:
-                        right-=1
-                    elif nums[left]+nums[right] < new_target:
-                        left+=1
+        ret = set()
+        if len(nums) < 4:
+            return []
+        for i in range(len(nums)-2):
+            for j in range(i+1,len(nums)-2,1):
+                l= j+1
+                r = len(nums)-1
+                while l<r:
+                    if nums[l]+nums[r]+ nums[i]+nums[j] <target:
+                        l=l+1
+                    elif nums[l]+nums[r]+ nums[i]+nums[j] >target:
+                        r = r-1
                     else:
-                        res.append([nums[i],nums[j],nums[left],nums[right]])
-                        temp_left = nums[left]
-                        temp_right = nums[right]
-                        while(left<right and nums[left]==temp_left):
-                            left+=1
-                        while(left<right and nums[right]==temp_right):
-                            right-=1
-                while j < L-3 and nums[j] == nums[j+1]:
-                    j+=1
-                j+=1
-            while i < L-4 and nums[i] == nums[i+1]:
-                i+=1
-            i+=1
-        return res        
+                        #ret.add((nums[i],nums[j],nums[l],nums[r]))
+                        if (nums[i],nums[j],nums[l],nums[r]) not in ret:
+                            ret.add((nums[i],nums[j],nums[l],nums[r]))
+                        l=l+1
+                        r = r-1
+         
+        return ret
+    
