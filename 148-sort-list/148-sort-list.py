031ab4b72ba338eeff04a3b0290e14cabfe3e9ba
Nikita Mandlik <nmandli1@binghamton.edu>
2022-06-28 14:36:14 -0400

    Time: 1891 ms (5.02%), Space: 36.5 MB (46.17%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -4,20 +4,41 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        new_node = head 
-        result = new_node
-        
-        arr =[]
-        
-        while head!= None:
-            arr.append(head.val)
-            head = head.next
-        
-        arr = sorted(arr)
-        
-        for i in arr:
-            new_node.val = i
-            new_node = new_node.next
+     def split(self, head):
+        prev = None
+        slow = fast = head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        if prev: prev.next = None
+        return slow
+    
+     def merge(self, l1, l2):
+        dummy = tail = ListNode()
+        while l1 or l2:
+            v1 = l1.val if l1 else inf
+            v2 = l2.val if l2 else inf
+            if v1 <= v2:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        return dummy.next
             
-        return result
+    
+     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head
+        head2 = self.split(head)
+        
+        head1 = self.sortList(head)
+        head2 = self.sortList(head2)
+        
+        return self.merge(head1, head2)
+    
+    #TC = O(n)
+    #SC = O(1)
+    
+    
