0cc9b90efa46c3ca09123e3cc8cfefe708db0f89
Nikita Mandlik <nmandli1@binghamton.edu>
2022-06-30 13:46:10 -0400

    Time: 82 ms (5.25%), Space: 13.8 MB (60.26%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,24 @@
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def Sumofsqr(n):
+            sum = 0
+            while n!=0:
+                if n/10 == 0:
+                    sqr = n*n
+                    sum = sum+sqr
+                    n = int(n/10)
+                
+                rem = n%10
+                sqr = rem*rem 
+                sum = sum + sqr
+                n = int(n/10)
+            return sum
+        res = set()
+        while n != 1 and n not in res:
+            res.add(n)
+            n = Sumofsqr(n)
+        return n == 1
+
+        
+    
