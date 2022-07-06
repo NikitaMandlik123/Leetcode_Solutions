8d9f876fe0041098c19651f8aad434f963dbdf6e
Nikita Mandlik <nmandli1@binghamton.edu>
2022-07-06 19:02:09 -0400

    Time: 101 ms (80.91%), Space: 20.8 MB (29.42%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,9 +1,18 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp = [0]
-        for i in range(1, n + 1):
-            if i % 2 == 1:
-                dp.append(dp[i - 1] + 1)
-            else:
-                dp.append(dp[i // 2])
-        return dp   
+        c = [0]
+        for i in range(1, n+1):
+            c.append(c[i >> 1] + i % 2)
+        return c
+    
+#     All whole numbers can be represented by 2N (even) and 2N+1 (odd).
+# For a given binary number, multiplying by 2 is the same as adding a zero at the end (just as we just add a zero when multiplying by ten in base 10).
+# Since multiplying by 2 just adds a zero, then any number and its double will have the same number of 1's. Likewise, doubling a number and adding one will increase the count by exactly 1. Or:
+
+# countBits(N) = countBits(2N)
+# countBits(N)+1 = countBits(2N+1)
+# Thus we can see that any number will have the same bit count as half that number, with an extra one if it's an odd number. We iterate through the range of numbers and calculate each bit count successively in this manner:
+
+#TC = O(n)
+#SC = O(1)
+
