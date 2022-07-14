092abc13b13e453e55620ed45b4b101a201d0eac
Nikita Mandlik <nmandli1@binghamton.edu>
2022-07-14 10:42:54 -0400

    Time: 106 ms (45.31%), Space: 16.2 MB (31.77%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        sol = []
+        for i in count:
+            if count[i]==1:
+                sol.append(i)
+        return sol
+        
