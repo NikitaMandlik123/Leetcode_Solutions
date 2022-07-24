37d97227cc5397f130511584eea866a9eb4d47a5
Nikita Mandlik <nmandli1@binghamton.edu>
2022-07-24 13:29:21 -0400

    Time: 202 ms (63.10%), Space: 18.1 MB (84.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -5,9 +5,7 @@ class Solution:
         #result
         res =[]
         for i in intervals:
-			# if the list of merged intervals is empty 
-			# or if the current interval does not overlap with the previous,
-			# simply append it.
+			
             if not res or res[-1][-1] < i[0]:
                 res.append(i)
 			# otherwise, there is overlap,
