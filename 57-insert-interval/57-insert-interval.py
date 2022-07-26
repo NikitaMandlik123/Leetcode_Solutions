77f177f9e1c26f09bb30b3aa7c61575edfd36691
Nikita Mandlik <nmandli1@binghamton.edu>
2022-07-26 08:33:29 -0400

    Time: 134 ms (40.62%), Space: 17.3 MB (53.14%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
+
+        solution = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not solution or solution[-1][1] < interval[0]:
+                solution.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                solution[-1][1] = max(solution[-1][1], interval[1])
+
+        return solution
