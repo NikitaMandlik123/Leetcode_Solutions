768a7f5885d0166968a789f31b39ed527bd2dd2d
Nikita Mandlik <nmandli1@binghamton.edu>
2022-07-25 21:10:34 -0400

    Time: 41 ms (70.58%), Space: 13.9 MB (23.89%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,21 +1,15 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        new1 = []
-        new2= []
-        
-        for ele in s:
-            if ele!="#":
-                new1.append(ele)
-            elif new1:
-                new1.pop()
-        for elem in t:
-            if elem!="#":
-                new2.append(elem)
-            elif new2:
-                new2.pop()
-                
-        if new1==new2:
-            return True
-        else:
-            return False
-        
+        return self.parse(s) == self.parse(t)
+    
+    def parse(self, x):
+        res = []
+        for c in x:
+            if c != "#":
+                res.append(c)
+            else:
+                if res: res.pop()
+        return res
+    
+    #TC = O(n)
+    #SC = O(n)
